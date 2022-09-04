19d09c667f0daf125ce0faae1c906de26e0b81c6
GB-Navarro <gbnavarro@usp.br>
2022-09-04 14:05:13 -0300

    refactor: the code of cardController has been refactored

--- controllers/cardController/cardController.ts
+++ controllers/cardController/cardController.ts
@@ -1,28 +1,19 @@
 import { Request, Response } from "express";
 
 import cardServices from "../../services/cardServices.js";
-export default async function cardController (req: Request,res: Response){
-    const {employeeId, cardType} = req.body;
-    const apiKey = req.headers.apikey;
-    const apiKeyOwnerExists = await cardServices.checkApiKeyOwnerExistence(apiKey.toString());
 
-    if(apiKeyOwnerExists){
-        const employeeExists = await cardServices.checkEmployeeExistence(employeeId);
-        if(employeeExists){
-            const employeeCardTypeExists = await cardServices.checkEmployeeCardTypeExistence(cardType, employeeId);
-            if(!(employeeCardTypeExists)){
-                const cardData: any = await cardServices.generateCard(employeeId, cardType);
-                await cardServices.createCard(cardData);
-                res.sendStatus(201);
-            }else{
-                res.sendStatus(404);
-            }
-        }else{
-            res.sendStatus(404);
-        }
-    }else{
-        console.log("a");
-        res.sendStatus(404);
-    }
-    
+export default async function cardController (req: Request,res: Response){
+
+    const {employeeId, cardType} = req.body;
+
+    const apiKey = req.headers.apikey;
+
+    await cardServices.checkApiKeyOwnerExistence(apiKey.toString());
+    await cardServices.checkEmployeeExistence(employeeId);
+    await cardServices.checkEmployeeCardTypeExistence(cardType, employeeId);
+
+    const cardData: any = await cardServices.generateCard(employeeId, cardType);
+    await cardServices.createCard(cardData);
+
+    res.sendStatus(201);
 }
