73faf4c89178cfed1b05953ff59bd144fa26b1c4
GB-Navarro <gbnavarro@usp.br>
2022-09-04 07:51:29 -0300

    feat: added a function to verify employee existence

--- controllers/cardController/cardController.ts
+++ controllers/cardController/cardController.ts
@@ -3,10 +3,18 @@ import { Request, Response } from "express";
 import cardServices from "../../services/cardServices.js";
 
 export default async function cardController (req: Request,res: Response){
+    //A api key do checkApiKeyOwnerExistence, deve vir do header
+    //A rota de criação do cartão, recebe do body o id do empregado e o tipo do cartão
+    const id = 2;
     const apiKeyOwnerExists = await cardServices.checkApiKeyOwnerExistence('zadKLNx.DzvOVjQH01TumGl2urPjPQSxUbf67vs0');
 
     if(apiKeyOwnerExists){
-        res.sendStatus(200);
+        const employeeExists = await cardServices.checkEmployeeExistence(id);
+        if(employeeExists){
+            res.sendStatus(200);
+        }else{
+            res.sendStatus(404);
+        }
     }else{
         res.sendStatus(404);
     }
--- services/cardServices.ts
+++ services/cardServices.ts
@@ -1,9 +1,10 @@
 import { findByApiKey } from "./../repositories/companyRepository.js"
+import { findById } from "./../repositories/employeeRepository.js";
 
 async function checkApiKeyOwnerExistence(apiKey: string){
 
     const result = await findByApiKey(apiKey);
-    
+
     let apiKeyOwnerExists:boolean;
 
     if(result === undefined){
@@ -15,8 +16,23 @@ async function checkApiKeyOwnerExistence(apiKey: string){
     }
 }
 
+async function checkEmployeeExistence(id: number){
+    const result = await findById(id);
+
+    let employeeExists:boolean;
+
+    if(result === undefined){
+        employeeExists = false;
+        return employeeExists;
+    }else{
+        employeeExists = true;
+        return employeeExists;
+    }
+}
+
 const cardServices = {
-    checkApiKeyOwnerExistence
+    checkApiKeyOwnerExistence,
+    checkEmployeeExistence
 }
 
 export default cardServices;
