ba59d02b480d22278afe1c1c53baa218ad45f876
GB-Navarro <gbnavarro@usp.br>
2022-09-05 09:41:58 -0300

    feat: added the purchasesRoutes to the server router

--- routes/router.ts
+++ routes/router.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import exampleRouter from "./exampleRouter/exampleRoutes.js";
 import cardRouter from "./cardRoutes.js";
+import purchasesRouter from "./purchasesRouter/purchasesRoutes.js";
 
 const router = Router();
 
 router.use(exampleRouter);
 router.use(cardRouter);
+router.use(purchasesRouter);
 
 export default router
